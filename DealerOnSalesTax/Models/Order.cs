a948a1c2e192da6501e21050517d79c6c6f95a8e
arunpsoftware <139647455+arunpsoftware@users.noreply.github.com>
2023-07-17 13:29:32 -0400

    backend logic wip

--- DealerOnSalesTax/Models/Order.cs
+++ DealerOnSalesTax/Models/Order.cs
@@ -3,19 +3,49 @@ namespace DealerOnSalesTax.Models
 {
 	public class OrderModel
 	{
+		private List<CartItem> _cartItems;
 		private List<OrderItem> _orderItems;
 
 		public OrderModel(List<CartItem> cartItems)
 		{
-			foreach (CartItem ci in cartItems)
-			{
-			}
+			_cartItems = cartItems;
+			_orderItems = new List<OrderItem>();
 		}
 
 		public string GetReceipt()
-		{
+        {
+			createOrderItemsFromCartItems();
+
+            foreach (OrderItem orderItem in _orderItems)
+            {
+
+            }
+
 			return "";
-		}
-	}
+        }
+
+		private void createOrderItemsFromCartItems()
+        {
+            foreach (CartItem cartItem in _cartItems)
+            {
+                string id = getId(cartItem);
+
+                if (_orderItems.Any(x => x.Id.Equals(id)))
+                {
+					_orderItems.FirstOrDefault(x => x.Id.Equals(id)).Quantity += cartItem.Quantity;
+                }
+                else
+                {
+                    OrderItem newOrderItem = new OrderItem(id, cartItem.Name, cartItem.Category, cartItem.IsImported, cartItem.Price, cartItem.Quantity);
+                    _orderItems.Add(newOrderItem);
+                }
+            }
+        }
+
+        private string getId(CartItem cartItem)
+        {
+            return string.Concat(cartItem.Name, cartItem.Category, cartItem.IsImported, cartItem.Price);
+        }
+    }
 }
 
--- DealerOnSalesTax/Models/OrderItem.cs
+++ DealerOnSalesTax/Models/OrderItem.cs
@@ -2,10 +2,33 @@
 namespace DealerOnSalesTax.Models
 {
 	public class OrderItem
-	{
-		public OrderItem()
+    {
+        // Readonly.
+        public string Id { get; }
+        public string Name { get; }
+        public string Category { get; }
+        public bool IsImported { get; }
+        public double Price { get; }
+        public bool HasBasicSalesTax { get; }
+
+        // Properties which can be changed.
+        public int Quantity { get; set; }
+
+        public OrderItem(string id, string name, string category, bool isImported, double price, int quantity)
 		{
-		}
+            Id = id;
+            Name = name;
+            Category = category;
+            IsImported = isImported;
+            Price = price;
+            HasBasicSalesTax = DoesCategoryHaveBasicSalesTax();
+            Quantity = quantity;
+        }
+
+        private bool DoesCategoryHaveBasicSalesTax()
+        {
+            return !Category.Equals("Uncategorized");
+        }
 	}
 }
 
