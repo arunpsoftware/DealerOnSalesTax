c40a690e1414563798aceb9f4bc261db72b0e957
arunpsoftware <139647455+arunpsoftware@users.noreply.github.com>
2023-07-16 21:39:00 -0400

    Create IndexViewModel.cs

---
+++ DealerOnSalesTax/Models/IndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DealerOnSalesTax.Models
+{
+	public class IndexViewModel
+	{
+		public List<OrderItem> OrderItems { get; set; }
+		public List<SelectListItem> Categories { get; set; }
+		public string Receipt { get; set; }
+	}
+
+	public class OrderItem
+	{
+		public string Name { get; set; }
+		public string Category { get; set; }
+		public bool IsImported { get; set; }
+		public double Price { get; set; }
+		public int Quantity { get; set; }
+    }
+}
+
