e392758dc82e20caf035b24678f813167a9a5d96
arunpsoftware <139647455+arunpsoftware@users.noreply.github.com>
2023-07-16 21:22:05 -0400

    init

---
+++ DealerOnSalesTax/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DealerOnSalesTax.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
